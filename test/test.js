dc37db1fb4391bf28d0246625d24c46b0cb4f754
Ansel Bridgewater <iamansel@gmail.com>
2019-07-25 17:10:45 -0700

    Removed mock, and wrote new test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 import Currency from '../src/index';
 
-jest.mock('../src/index');
-
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  Currency.mockClear();
-});
-
 it('expects 2 + 2 to equal 4', () => {
   expect(2 + 2).toBe(4);
+});
+
+it('expects Currency to only allow integers', () => {
+  const n = 15.30
+
+  expect(() => {
+    const c = new Currency(n);
+  }).toThrow(TypeError(`Expected integer representing pennies, received ${n}`));
 });
