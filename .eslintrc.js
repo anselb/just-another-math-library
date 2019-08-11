173aefd348e224857102be6a4a49c62d7a717d09
Ansel Bridgewater <iamansel@gmail.com>
2019-08-10 18:04:00 -0700

    Added more linter stuff for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,23 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint'
+  ],
   rules: {
   },
 };
